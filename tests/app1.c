6812f042671e843747e9e3edaced375ba992d33c
iljoo baek <paek1ju@gmail.com>
2019-05-14 18:04:08 -0400

    new test

--- tests/app1.c
+++ tests/app1.c
@@ -1,19 +1,21 @@
 #include <stdio.h> //fprintf
 #include <unistd.h> // sleep()
-#include "tag_gpu.h" 
-int main(int argc, char *argv){ 
+#include "tag_gpu.h"
+int main(int argc, char *argv){
 
-	for (int i = 0; i < 10; i++)
+	for (int i = 0; i < 10000; i++)
 	{
-		fprintf(stdout, "opengl_get_image: tag_beginning()\n");
+		fprintf(stdout, "opencv: tag_beginning() %d\n", i);
 		// Tagging begin ///////////////////////////////////////////////////////////////////
-		const char *task_name = "opengl_get_image";
+		const char *task_name = "opencv_get_image";
 		tag_begin(1, task_name, 0L, 0L, 0L, 0.0, 0.0, 0.0, 0, 0);
+
+        usleep(1000);
 
 		// Tag_end /////////////////////////////////////////////////////////////////////////
 		tag_end(1, task_name);
 
-		fprintf(stdout, "opengl_get_image: onto next job in 1s\n");
-		sleep(1);
+		fprintf(stdout, "opencv_get_image: onto next job in 1s\n");
+		usleep(1000);
 	}
 }

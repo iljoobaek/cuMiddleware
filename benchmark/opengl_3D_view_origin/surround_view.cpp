3b84bcf4aa0e0d3a1f298c66c057620702068e30
iljoo baek <paek1ju@gmail.com>
2019-09-21 23:24:33 -0400

    Print avg worst fps

--- benchmark/opengl_3D_view_origin/surround_view.cpp
+++ benchmark/opengl_3D_view_origin/surround_view.cpp
@@ -30,6 +30,7 @@ using namespace glm;
 #include <opencv/cv.h>
 #include <opencv/highgui.h>
 #include <opencv2/imgproc/imgproc.hpp>
+using namespace cv;
 
 #include "shader.hpp"
 #include "texture.hpp"

d43d28112458ce2e54e523d85730cc1c53f2b060
iljoo baek <paek1ju@gmail.com>
2019-10-14 11:53:01 -0400

    overhead test

--- benchmark/benchmark_test.sh
+++ benchmark/benchmark_test.sh
@@ -1,12 +1,6 @@
 
 
-0.023 * 1000 = 23
-
-1000 /23 = 
-
-
-
-##### LSF
+##### LSF Test on GTX 1070
 sudo taskset -c 1 schedtool -F -p 60 -e ./mid --policy lsf
 
 # tag layer depth 0
@@ -19,7 +13,7 @@ sudo LD_LIBRARY_PATH=../../lib taskset -c 4 schedtool -F -p 52 -e ./surround_vie
 sudo LD_LIBRARY_PATH=./:../../lib taskset -c 6 schedtool -F -p 52 -e ./cuTestClient
 
 
-##### Tag-Only
+##### Tag-Only Test on GTX 1070
 sudo taskset -c 1 schedtool -F -p 60 -e ./mid --policy fifo
 
 # tag layer depth 0
@@ -32,5 +26,52 @@ sudo LD_LIBRARY_PATH=../../lib taskset -c 4 schedtool -F -p 52 -e ./surround_vie
 sudo LD_LIBRARY_PATH=./:../../lib taskset -c 6 schedtool -F -p 52 -e ./cuTestClient
 
 
+##### Overhead Test on GTX 1070
 
-export LD_LIBRARY_PATH=/home/iljoo/cuMiddleware_work/cuMiddleware/lib:$LD_LIBRARY_PATH
+## Opengl Tutorial17 - No CARSS
+./cuTestClient
+-> We run the graphics task without real-time prioirity to avoid blocking rendering
+## Opengl Tutorial17 - CARSS - Tag-only
+sudo taskset -c 1 schedtool -F -p 60 -e ./mid --policy fifo
+sudo LD_LIBRARY_PATH=./:../../lib taskset -c 6 schedtool -F -p 52 -e ./cuTestClient
+## Opengl Tutorial17 - CARSS - lsf
+sudo taskset -c 1 schedtool -F -p 60 -e ./mid --policy lsf
+sudo LD_LIBRARY_PATH=./:../../lib taskset -c 6 schedtool -F -p 52 -e ./cuTestClient
+-> FrameController fc(frame_name, 60, false);
+-> It is NOT ideal method to measure the overhead...
+
+
+## Opengl 3D surround view - No CARSS
+./surround_view
+-> We run the graphics task without real-time prioirity to avoid blocking rendering
+## Opengl 3D surround view - CARSS - Tag-only
+./mid --policy fifo
+./surround_view
+-> We run the graphics task without real-time prioirity to avoid blocking rendering
+## Opengl 3D surround view - CARSS - lsf
+./mid --policy lsf
+./surround_view
+-> set FrameController fc(frame_name, 30, false);
+-> We run the graphics task without real-time prioirity to avoid blocking rendering
+
+
+## PyTorch SSD MobileNet - No CARSS
+sudo -E LD_LIBRARY_PATH=../../lib taskset -c 2 schedtool -F -p 52 -e python3.6 detect_ssd.py
+## PyTorch SSD MobileNet - CARSS - Tag-only
+sudo taskset -c 1 schedtool -F -p 60 -e ./mid --policy fifo
+sudo -E LD_LIBRARY_PATH=../../lib taskset -c 2 schedtool -F -p 58 -e python3.6 detect_ssd.py --fps 60
+-> depth 0 - frame level tagging
+## PyTorch SSD MobileNet - CARSS - lsf
+sudo taskset -c 1 schedtool -F -p 60 -e ./mid --policy lsf
+sudo -E LD_LIBRARY_PATH=../../lib taskset -c 2 schedtool -F -p 58 -e python3.6 detect_ssd.py --fps 60
+-> depth 0 - frame level tagging
+
+
+## OpenCV Hog - NO CARSS
+sudo LD_LIBRARY_PATH=../../lib taskset -c 3 schedtool -F -p 52 -e ./hog
+## OpenCV Hog - CARSS - Tag-Only
+sudo taskset -c 1 schedtool -F -p 60 -e ./mid --policy fifo
+sudo LD_LIBRARY_PATH=../../lib taskset -c 3 schedtool -F -p 52 -e ./hog
+## OpenCV Hog - CARSS - Tag-Only
+sudo taskset -c 1 schedtool -F -p 60 -e ./mid --policy lsf
+sudo LD_LIBRARY_PATH=../../lib taskset -c 3 schedtool -F -p 52 -e ./hog 30

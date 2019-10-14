8df2d53eb0ad77cb92c041c848a49f78644dbb25
iljoo baek <paek1ju@gmail.com>
2019-10-14 13:18:18 -0400

    overhead test fixed

--- benchmark/benchmark_test.sh
+++ benchmark/benchmark_test.sh
@@ -1,32 +1,33 @@
 
 
-##### LSF Test on GTX 1070
+########## LSF Test on GTX 1070
 sudo taskset -c 1 schedtool -F -p 60 -e ./mid --policy lsf
 
 # tag layer depth 0
 sudo -E LD_LIBRARY_PATH=../../lib taskset -c 2 schedtool -F -p 58 -e python3.6 detect_ssd.py --fps 60
-
 sudo LD_LIBRARY_PATH=../../lib taskset -c 3 schedtool -F -p 56 -e ./hog 30
-
 sudo LD_LIBRARY_PATH=../../lib taskset -c 4 schedtool -F -p 52 -e ./surround_view
-
 sudo LD_LIBRARY_PATH=./:../../lib taskset -c 6 schedtool -F -p 52 -e ./cuTestClient
 
 
-##### Tag-Only Test on GTX 1070
+########## Tag-Only Test on GTX 1070
 sudo taskset -c 1 schedtool -F -p 60 -e ./mid --policy fifo
 
 # tag layer depth 0
 sudo -E LD_LIBRARY_PATH=../../lib taskset -c 2 schedtool -F -p 52 -e python3.6 detect_ssd.py --fps 50
-
 sudo LD_LIBRARY_PATH=../../lib taskset -c 3 schedtool -F -p 52 -e ./hog
-
 sudo LD_LIBRARY_PATH=../../lib taskset -c 4 schedtool -F -p 52 -e ./surround_view
+sudo LD_LIBRARY_PATH=../:../../lib taskset -c 6 schedtool -F -p 52 -e ./cuTestClient
 
-sudo LD_LIBRARY_PATH=./:../../lib taskset -c 6 schedtool -F -p 52 -e ./cuTestClient
+
+########## tensorflow example
+source dnn_env/bin/activate
+python3.5 detector.py
+deactivate
 
 
-##### Overhead Test on GTX 1070
+
+########## Overhead Test on GTX 1070
 
 ## Opengl Tutorial17 - No CARSS
 ./cuTestClient
